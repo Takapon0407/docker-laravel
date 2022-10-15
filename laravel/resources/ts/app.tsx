fd89e1e79c5a7623d552d430e727b597eb0974dc
Takapon0407 <takapon_business@yahoo.co.jp>
2022-10-15 23:57:41 +0900

    微修正

--- laravel/resources/ts/app.tsx
+++ laravel/resources/ts/app.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <div>
-        <h1>Laravel + Vite + React</h1>
-    </div>
+    <React.StrictMode>
+        <div>
+            <h1>Laravel + Vite + React</h1>
+        </div>
+    </React.StrictMode>
 );
