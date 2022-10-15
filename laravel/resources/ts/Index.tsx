2977dfd80cc6cbf4a9d4bc45339a37f3f8374a23
Takapon0407 <takapon_business@yahoo.co.jp>
2022-10-15 23:08:16 +0900

    tsフォルダを切ってそちらへ移動

---
+++ laravel/resources/ts/Index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <div>
+            <h1>Laravel + Vite + React</h1>
+        </div>
+    );
+};
+
+export default Index;
---
+++ laravel/resources/ts/app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <div>
+        <h1>Laravel + Vite + React</h1>
+    </div>
+);
