f1a96dfd714e1b23565c87f13505ed9a45de9dce
Takapon0407 <takapon_business@yahoo.co.jp>
2022-10-15 23:08:41 +0900

    laravelのルーティングで/react/の場合はspa構成になるように

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/react/{any}', function () {
+    return view('index');
+})->where('any', '.*');
