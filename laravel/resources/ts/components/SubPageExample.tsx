1833f8d58da4aca1c30e78e7f2286dc652500987
Takapon0407 <takapon_business@yahoo.co.jp>
2022-10-16 01:42:27 +0900

    Home, Sub, 404想定の簡易表示を追加

---
+++ laravel/resources/ts/components/SubPageExample.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SubPageExample = () => (
+    <div>
+        <h2>SubPageExample!</h2>
+        My SubPageExample page!
+    </div>
+);
+
+export default SubPageExample;
