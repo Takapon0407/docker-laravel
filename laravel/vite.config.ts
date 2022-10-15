ed529c77c38836196d1a2fb151d21ec0d7700811
Takapon0407 <takapon_business@yahoo.co.jp>
2022-10-15 23:57:07 +0900

    拡張子をtsへ

--- laravel/vite.config.ts
+++ laravel/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,11 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/ts/app.tsx"],
             refresh: true,
         }),
+        react(),
     ],
+    resolve: {
+        alias: {
+            "@": "/resources/ts",
+        },
+    },
 });
