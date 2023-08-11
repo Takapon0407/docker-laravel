94f52be5e749b82c1eb4ec8bf0e62c85be3e6526
Takapon0407 <takapon_business@yahoo.co.jp>
2023-08-12 01:07:12 +0900

    homeに簡単なアイテムを追加

--- laravel/resources/ts/components/Home.tsx
+++ laravel/resources/ts/components/Home.tsx
@@ -4,6 +4,13 @@ const Home = () => (
     <div>
         <h2>Home</h2>
         My Home page!
+        <ul>
+            <li>item-1</li>
+            <li>item-2</li>
+            <li>item-3</li>
+            <li>item-4</li>
+            <li>item-5</li>
+        </ul>
     </div>
 );
 
