1cfbb81d3176efc8ed1a1e1c9b9e41eb35f817db
Takapon0407 <takapon_business@yahoo.co.jp>
2023-08-12 03:39:18 +0900

    テスト用のデータとtsを読み込めるように

--- laravel/resources/views/layouts/header.blade.php
+++ laravel/resources/views/layouts/header.blade.php
@@ -1,12 +1,15 @@
+@vite('resources/ts/laravel/sample.ts')
 <nav class="navbar navbar-expand-lg navbar-light pl-5 pr-5 pt-2 pb-2">
   <a class="navbar-brand text-white ms-3" href="/">TektekTech</a>
   <div class="collapse navbar-collapse" id="Navber">
     <ul class="navbar-nav">
-      <li class="nav-item ml-2">
-        <a href="#" class="nav-link dropdown-toggle text-white" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ホーム</a>
-      </li>
-      <li class="nav-item ml-2">
+      <li class="nav-item ml-2 dropdown">
         <a href="#" class="nav-link dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">コミュニティ</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">ほげ</a>
+          <a class="dropdown-item" href="#">ふご</a>
+          <a class="dropdown-item" href="#">ほご</a>
+        </div>
       </li>
     </ul>
     <ul class="navbar-nav ml-auto mr-5">
@@ -14,7 +17,7 @@
         <a class="nav-link text-white" href="#">ストック一覧</a>
       </li>
       <li class="nav-item ml-2">
-        <a class="nav-link text-white" id="post-link" href="/drafts/new">投稿する</a>
+        <a class="nav-link text-white" id="history" href="/history">History</a>
       </li>
       <li class="nav-item ml-2">
         <a class="nav-link text-white" id="register" href="/register">ユーザ登録</a>
@@ -22,4 +25,3 @@
     </ul>
   </div><!-- /.navbar-collapse -->
 </nav>
-
