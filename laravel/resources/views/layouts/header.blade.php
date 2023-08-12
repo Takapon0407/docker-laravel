c66a07600c44489ff31544b3e48e9c49be0b9f47
Takapon0407 <takapon_business@yahoo.co.jp>
2023-08-12 14:20:19 +0900

    ボタンの色をカスタマイズして、ドロップダウンの例が表示されるように

--- laravel/resources/views/layouts/header.blade.php
+++ laravel/resources/views/layouts/header.blade.php
@@ -1,15 +1,20 @@
 @vite('resources/ts/laravel/sample.ts')
+@vite('resources/sass/app.scss')
 <nav class="navbar navbar-expand-lg navbar-light pl-5 pr-5 pt-2 pb-2">
   <a class="navbar-brand text-white ms-3" href="/">TektekTech</a>
   <div class="collapse navbar-collapse" id="Navber">
     <ul class="navbar-nav">
       <li class="nav-item ml-2 dropdown">
-        <a href="#" class="nav-link dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">コミュニティ</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">ほげ</a>
-          <a class="dropdown-item" href="#">ふご</a>
-          <a class="dropdown-item" href="#">ほご</a>
-        </div>
+      <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </button>
+        <ul class="dropdown-menu">
+          <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+          <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+          <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+        </ul>
+      </div>
       </li>
     </ul>
     <ul class="navbar-nav ml-auto mr-5">
@@ -23,5 +28,5 @@
         <a class="nav-link text-white" id="register" href="/register">ユーザ登録</a>
       </li>
     </ul>
-  </div><!-- /.navbar-collapse -->
+  </div>
 </nav>
