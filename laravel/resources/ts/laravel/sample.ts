2d89ec87e19bcbeca7e77d24088b8a73c4a18515
Takapon0407 <takapon_business@yahoo.co.jp>
2023-08-12 14:21:08 +0900

    jsを読み込む一例として、console.logのみ行うjsを作成

--- laravel/resources/ts/laravel/sample.ts
+++ laravel/resources/ts/laravel/sample.ts
@@ -1,10 +1,3 @@
-import * as bootstrap from "bootstrap";
+import "bootstrap";
 
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdownToggles = document.querySelectorAll(".dropdown-toggle");
-    dropdownToggles.forEach(function (toggle) {
-        new bootstrap.Dropdown(toggle);
-    });
-});
-
-console.log("Hoge");
+console.log("Hello World");
