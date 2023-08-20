8c8765f47a559ff97f972f62364b03c49e832ba7
Takapon0407 <takapon_business@yahoo.co.jp>
2023-08-20 21:16:00 +0900

    簡易的にプロフィールのだれ？の内容を入力し、誕生日ベースで年齢が表示されるように

--- laravel/resources/views/portfolio.blade.php
+++ laravel/resources/views/portfolio.blade.php
@@ -7,8 +7,19 @@
     <div class="content-box">
         <h2 class="font-cherry">だれ？</h2>
         <div class="sub-content">
-            <div class="sub-left-content">Webエンジニアをやっています。新しいこと・しょうもないことが大好物です。</div>
-            <div class="sub-right-content">right</div>
+            <div class="sub-left-content">
+                <img src="https://tektektech.com/wp-content/uploads/2020/09/48579941.423B85AD1BAF42FC9C624A1F2D8422CE.19080902.jpg"
+                    alt="profile img"
+                    title="profile img"
+                    width="100%"
+                    height="100%"
+                >
+            </div>
+            <div class="sub-right-content">
+                <div>名前: たかぽん</div>
+                <div id="age">年齢: 計算中...</div>
+                <div>趣味: カメラ, 将棋, ブログ...等</div>
+            </div>
         </div>
     </div>
     <div class="content-box">
