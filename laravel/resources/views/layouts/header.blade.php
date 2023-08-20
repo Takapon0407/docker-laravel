ff49c18523452366ebc81d0bafb8313a0a9a75ef
Takapon0407 <takapon_business@yahoo.co.jp>
2023-08-20 23:09:35 +0900

    順序は入れ替えておく

--- laravel/resources/views/layouts/header.blade.php
+++ laravel/resources/views/layouts/header.blade.php
@@ -3,6 +3,11 @@
 <nav class="navbar navbar-expand-lg navbar-light pl-5 pr-5 pt-2 pb-2">
   <a class="navbar-brand text-white ms-3" href="/">TektekTech</a>
   <div class="collapse navbar-collapse" id="Navber">
+    <ul class="navbar-nav ml-auto mr-5">
+      <li class="nav-item ml-2">
+        <a class="nav-link text-white" id="portfolio" href="/portfolio">Portfolio</a>
+      </li>
+    </ul>
     <ul class="navbar-nav">
       <li class="nav-item ml-2 dropdown">
       <div class="dropdown">
@@ -15,11 +20,6 @@
           <li><button class="dropdown-item" type="button">Dropdown item 3</button></li>
         </ul>
       </div>
-      </li>
-    </ul>
-    <ul class="navbar-nav ml-auto mr-5">
-      <li class="nav-item ml-2">
-        <a class="nav-link text-white" id="portfolio" href="/portfolio">Portfolio</a>
       </li>
     </ul>
   </div>
