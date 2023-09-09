f0db5455c1a0095c286a3fbcffa3cf75f1cb0551
Takapon0407 <takapon_business@yahoo.co.jp>
2023-09-09 22:55:31 +0900

    app.scssにてimportして利用する想定のため、ロード部分は削除し、ハンバーガーメニューが表示されるように修正

--- laravel/resources/views/layouts/header.blade.php
+++ laravel/resources/views/layouts/header.blade.php
@@ -1,7 +1,17 @@
 @vite('resources/ts/laravel/sample.ts')
-@vite('resources/sass/app.scss')
 <nav class="navbar navbar-expand-lg navbar-light pl-5 pr-5 pt-2 pb-2">
   <a class="navbar-brand text-white ms-3" href="/">TektekTech</a>
+  <button 
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#Navber"
+    aria-controls="Navber"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
   <div class="collapse navbar-collapse" id="Navber">
     <ul class="navbar-nav ml-auto mr-5">
       <li class="nav-item ml-2">
