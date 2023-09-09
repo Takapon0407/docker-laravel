95491f4cd89eaa443aef45a70ca5e75a06705c83
Takapon0407 <takapon_business@yahoo.co.jp>
2023-09-09 23:24:10 +0900

    月と日も追加

--- laravel/resources/ts/laravel/profile.ts
+++ laravel/resources/ts/laravel/profile.ts
@@ -2,10 +2,16 @@ import "bootstrap";
 import moment from "moment";
 
 const currentDate = moment();
+const birthDate = moment("1994-04-07");
+const ageYears = currentDate.diff(birthDate, "years");
+birthDate.add(ageYears, "years");
 
-// 年齢を計算して表示
-const age = currentDate.diff(moment("1994-04-07"), "years");
+const ageMonths = currentDate.diff(birthDate, "months");
+birthDate.add(ageMonths, "months");
+
+const ageDays = currentDate.diff(birthDate, "days");
+
 const ageElement = document.getElementById("age");
 if (ageElement) {
-    ageElement.innerText = "年齢: " + age;
+    ageElement.innerText = `年齢: ${ageYears}歳 ${ageMonths}ヶ月 ${ageDays}日`;
 }
