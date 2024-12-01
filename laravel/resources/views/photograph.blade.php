a80104c5e045e20bd07782afd042075567af9ed7
Takapon0407 <takapon_business@yahoo.co.jp>
2024-12-01 22:13:42 +0900

    見づらいのでスペースを入れておく

--- laravel/resources/views/photograph.blade.php
+++ laravel/resources/views/photograph.blade.php
@@ -8,6 +8,7 @@
 <link
     rel="stylesheet"
     href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 
 <div class="container">
