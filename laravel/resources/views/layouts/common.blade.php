0c0c9630f558ddde48493eee18c507e20e0e3f8a
Takapon0407 <takapon_business@yahoo.co.jp>
2025-10-17 04:36:18 +0900

    PWA対応のために必要な修正を一通り反映

--- laravel/resources/views/layouts/common.blade.php
+++ laravel/resources/views/layouts/common.blade.php
@@ -7,6 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- responsiveにするために追加 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- PWAに対応するために追加 -->
+    @PwaHead
     <title>てくてくテック</title>
     <!-- cssをインポート -->
     @vite('resources/sass/app.scss')
--- laravel/resources/views/top.blade.php
+++ laravel/resources/views/top.blade.php
@@ -1,3 +1,6 @@
 @extends('layouts.common')
 @section('content')
+<div style="display: flex; justify-content: center; align-items: center; height: 80vh;">
+    <h1 style="color: blue; font-size: 4rem; font-weight: bold;">Hello World</h1>
+</div>
 @endsection
